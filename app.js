f57debf0689a2ad7fbecae7f0ed5ea29b587eb25
gokulramr <42071885+g0kulram@users.noreply.github.com>
2025-11-22 15:34:22 +0000

    Implement operate function to select the right operation

--- app.js
+++ app.js
@@ -17,3 +17,26 @@ function divide(num1, num2) {
 let leftOperand = null;
 let operator = null;
 let rightOperand = null;
+
+function operate(left, operation, right) {
+    switch (operation) {
+        case '+':
+            add(left, right);
+            break;
+
+        case '-':
+            subtract(left, right);
+            break;
+
+        case '*':
+            multiply(left, right);
+            break;
+
+        case '/':
+            divide(left, right);
+            break;
+
+        default:
+            break;
+    }
+}
